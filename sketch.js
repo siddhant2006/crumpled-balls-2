dbb97d9c0616260bbfa6d92e020a971744e507a0
siddhant2006 <68522432+siddhant2006@users.noreply.github.com>
2020-09-18 20:51:54 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+
+
+   const Engine = Matter.Engine;
+   const World = Matter.World;
+   const Bodies = Matter.Bodies;
+   const Body = Matter.Body;
+   
+   var groundObject,paperObject,dustbinObj;	
+   var world;
+
+   
+   
+   function setup() {
+	   createCanvas(1600, 700);
+	   rectMode(CENTER);
+   
+   
+	   engine = Engine.create();
+	   world = engine.world;
+	   dustbinObj=new Dustbin(1200,650);
+	   paperObject=new Paper(250,350,70);
+	   groundObject=new Ground(width/2,690,width,10);
+	   
+	  
+	   
+	   
+	   //Create a Ground
+	   
+   
+	   
+   
+	   Engine.run(engine);
+   
+	 
+   }
+   
+   
+   function draw() {
+	 rectMode(CENTER);
+	 background(180);
+	 paperObject.display();
+	 dustbinObj.display();
+	 
+	 groundObject.display();
+
+	 
+
+	
+	
+	
+	 
+	
+	 
+	 
+	
+   }
+   
+   function keyPressed() {
+		 if (keyCode === UP_ARROW) {
+   
+		   Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:255,y:-250});
+	   
+		 }
+   }
+
+
+
+ 
